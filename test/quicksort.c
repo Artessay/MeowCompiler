83e5e8a7838e650052e31c744267c751e72542a9
Artessay <rihong.edu@outlook.com>
2023-04-07 19:16:02 +0800

    quicksort pass

--- test/quicksort.c
+++ test/quicksort.c
@@ -6,6 +6,9 @@ int scanf(const char *__format, ...);
 
 int printf(const char *__format, ...);
 
+#define MaxN 10000
+int array[MaxN];
+
 void quickSort(int left, int right, int arr[]) {
     int i = left, j = right;
     int pivot = arr[(left + right) / 2];
@@ -31,20 +34,19 @@ void quickSort(int left, int right, int arr[]) {
 
 int main() {
     int n;
-    int *array;
-
+    
     scanf("%d", &n);
-    array = (int *)malloc(n*sizeof(int));
+    // array = (int *)malloc(n*sizeof(int));
     for (int i = 0; i < n; ++i) {
         scanf("%d", array + i);
     }
     
-    quickSort(0, n, array);
+    quickSort(0, n-1, array);
 
     for (int i = 0; i < n; ++i) {
         printf("%d\n", array[i]);
     }
 
-    free(array);
+    // free(array);
     return 0;
 }
