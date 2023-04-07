db08e28c2fa4aa6e4609d689068f7bd4c218fb30
Artessay <rihong.edu@outlook.com>
2023-04-07 15:22:44 +0800

    first test writing

---
+++ test/quicksort.c
@@ -0,0 +1,50 @@
+void *malloc(unsigned long long _Size);
+
+void free(void *_Memory);
+
+int scanf(const char *__format, ...);
+
+int printf(const char *__format, ...);
+
+void quickSort(int left, int right, int arr[]) {
+    int i = left, j = right;
+    int pivot = arr[(left + right) / 2];
+
+    while (i <= j) {
+        while (arr[i] < pivot)
+            ++i;
+        while (arr[j] > pivot)
+            --j;
+        if (i <= j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            ++i; --j;
+        }
+    }
+
+    if (left < j)
+        quickSort(left, j, arr);
+    if (i < right)
+        quickSort(i, right, arr);
+}
+
+int main() {
+    int n;
+    int *array;
+
+    scanf("%d", &n);
+    array = (int *)malloc(n*sizeof(int));
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", array + i);
+    }
+    
+    quickSort(0, n, array);
+
+    for (int i = 0; i < n; ++i) {
+        printf("%d\n", array[i]);
+    }
+
+    free(array);
+    return 0;
+}
