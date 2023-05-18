a99dd3df86fef8ba0300dbf0de35baf6599912d4
Artessay <rihong.edu@outlook.com>
2023-05-18 20:14:19 +0800

    save

--- src/test.c
+++ src/test.c
@@ -1,11 +1,11 @@
-// int add(int a, int b) {
-//     return a + b;
-// }
+int add(int a, int b) {
+    return a + b;
+}
 
 int main() {
     int n;
     n = 1 + 2;
-    // n = 3 * 4 - add(1, 2);
+    n = 3 * 4 - add(1, 2);
 
     return n;
 }
