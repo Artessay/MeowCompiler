94f03e035dfe55225fab68d8e057150f7b1b4e34
Artessay <rihong.edu@outlook.com>
2023-05-18 23:18:41 +0800

    recover

--- src/ParseTree.h
+++ src/ParseTree.h
@@ -53,12 +53,11 @@ struct A_topClauseList_ {
     A_topClauseList next;
 };
 
-typedef enum {
-    A_Preprocess, A_FunctionDeclare, A_GlobalVarDefine
-} A_topClauseKind;
 
 struct A_topClause_ {
-    A_topClauseKind kind;
+    enum {
+        A_Preprocess, A_FunctionDeclare, A_GlobalVarDefine
+    } kind;
     A_pos pos;
     union {
         A_funcDeclare function;
