02392b7c15105fb4542703cad1e9f0eb0903ad15
Artessay <rihong.edu@outlook.com>
2023-05-18 23:27:11 +0800

    start semantic

---
+++ src/semantic.c
@@ -0,0 +1,11 @@
+#include "semantic.h"
+
+#include <stdio.h>
+
+#include <llvm-c/Core.h>
+#include <llvm-c/ExecutionEngine.h>
+#include <llvm-c/Target.h>
+
+void SEM_transProgram(A_topClause program) {
+    ;
+}
---
+++ src/semantic.h
@@ -0,0 +1,8 @@
+#ifndef _SEMANTIC_H_
+#define _SEMANTIC_H_
+
+#include "ParseTree.h"
+
+void SEM_transProgram(A_topClause program);
+
+#endif
