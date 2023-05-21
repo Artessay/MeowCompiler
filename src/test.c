0d28da97f3b25e10ea2ccb733a1178d56070a581
Artessay <rihong.edu@outlook.com>
2023-05-21 22:37:27 +0800

    tests

--- src/test.c
+++ src/test.c
@@ -4,11 +4,65 @@
 
 int printf(char *__format, ...);
 
-int main() {
-    int T = 10;
-    while (T != 0) {
-        printf("%d\n", T);
-        T = T - 1;
+// int array[10000];
+int array[10];
+
+void quickSort(int left, int right, int* arr) {
+    int i = left, j = right;
+    int pivot = arr[(left + right) / 2];
+
+    while (i <= j) {
+        while (arr[i] < pivot){
+            // ++i;
+            i = i + 1;
+        }            
+        while (arr[j] > pivot){
+            // --j;
+            j = j - 1;
+        }  
+        if (i <= j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            // ++i; --j;
+            i = i + 1;
+            j = j - 1;
+        }
     }
+
+    if (left < j){
+        quickSort(left, j, arr);
+    }
+    if (i < right){
+        quickSort(i, right, arr);
+    }
+}
+
+int main() {
+    // int n = 10;
+    // scanf("%d", &n);
+
+    // for (int i = 0; i < n; ++i) {
+    //     scanf("%d", array + i);
+    // }
+
+    int n = 4;
+    array[0] = 3; 
+    array[1] = 6;
+    array[2] = 5;
+    array[3] = 9;
+    // array[4] = 2;
+    // array[5] = 1;
+    // array[6] = 0;
+    // array[7] = 4;
+    // array[8] = 8;
+    // array[9] = 7;
+
+    quickSort(0, n - 1 , array);
+
+    for (int i = 0; i < n; i = i + 1) {
+        printf("%d\n", array[i]);
+    }
+
     return 0;
 }
---
+++ testMeow/test3.c
@@ -0,0 +1,11 @@
+int main() {
+    int n = 10;
+    int x;
+    if (n < 6) {
+        x = 1;
+    } else {
+        x = 2;
+    }
+
+    return x;
+}
