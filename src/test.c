8698107811d4ecf94418ed97d8160baaf9471175
Artessay <rihong.edu@outlook.com>
2023-05-22 10:14:10 +0800

    try matrix

--- src/test.c
+++ src/test.c
@@ -2,49 +2,44 @@ int scanf(char *__format, ...);
 
 int printf(char *__format, ...);
 
-int array[10000];
+int a[25][25], b[25][25], c[25][25];
 
-int quickSort(int left, int right) {
-    int i = left, j = right;
-    int pivot = array[(left + right) / 2];
-
-    while (i <= j) {
-        while (array[i] < pivot){
-            ++i;
-        }            
-        while (array[j] > pivot){
-            --j;
-        }  
-        if (i <= j) {
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            ++i; --j;
+int main() {
+    int ra, ca;
+    scanf("%d%d", &ra, &ca);
+    for (int i = 0; i < ra; ++i) {
+        for (int j = 0; j < ca; ++j) {
+            scanf("%d", &a[i][j]);
         }
     }
 
-    if (left < j){
-        quickSort(left, j);
+    int rb, cb;
+    scanf("%d%d", &rb, &cb);
+    for (int i = 0; i < rb; ++i) {
+        for (int j = 0; j < cb; ++j) {
+            scanf("%d", &b[i][j]);
+        }
     }
-    if (i < right){
-        quickSort(i, right);
+
+    if (ca != rb) {
+        printf("Incompatible Dimensions\n");
+        return 0;
     }
 
-    return 0;
-}
+    for (int i = 0; i < ra; ++i) {
+        for (int j = 0; j < cb; ++j) {
+            c[i][j] = 0;
+            for (int k = 0; k < ca; ++k) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
 
-int main() {
-    int n;
-    scanf("%d", &n);
-
-    for (int i = 0; i < n; i = i + 1) {
-        scanf("%d", &array[i]);
-    }   
-
-    quickSort(0, n - 1);
-
-    for (int i = 0; i < n; i = i + 1) {
-        printf("%d\n", array[i]);
+    for (int i = 0; i < ra; ++i) {
+        for (int j = 0; j < cb; ++j) {
+            printf("%10d", c[i][j]);
+        }
+        printf("\n");
     }
 
     return 0;
